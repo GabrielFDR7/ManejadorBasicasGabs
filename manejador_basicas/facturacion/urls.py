da12eee039d05fb8094e44700aaacb946e6fa266
Gabriel Dicelis <g.dicelis@uniandes.edu.co>
2023-09-30 06:00:44 -0500

    cambios

--- manejador_basicas/facturacion/urls.py
+++ manejador_basicas/facturacion/urls.py
@@ -4,7 +4,7 @@ from django.views.decorators.csrf import csrf_exempt
 from facturacion import views
 
 urlpatterns = [
-    path('servicios/', views.servicio_list, name='servicios'),
-    path('crearServicio/', views.servicio_create, name='servicioCreate'),
+    path('/servicios/', views.servicio_list, name='servicios'),
+    path('/crearServicio/', views.servicio_create, name='servicioCreate'),
     # Otras rutas aquí...
 ]
--- manejador_basicas/facturacion/views.py
+++ manejador_basicas/facturacion/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .forms import ServicioForm
 from django.contrib import messages
 from django.http import HttpResponseRedirect
@@ -6,19 +6,21 @@ from django.urls import reverse
 from .logic.factura_logic import create_servicio, get_servicio
 
 def servicio_list(request):
-    servicios = get_servicio()
+    servicios = get_servicio()  # Obtén la lista actualizada de servicios
     context = {
         'servicio_list': servicios
     }
+    print(servicios)
     return render(request, 'servicios.html', context)
 
-def servicio_create(request):
+
+def crear_servicio(request):
     if request.method == 'POST':
         form = ServicioForm(request.POST)
         if form.is_valid():
             create_servicio(form)
-            messages.add_message(request, messages.SUCCESS, 'Servicio create successful')
-            return HttpResponseRedirect(reverse('ServicioCreate'))
+            messages.success(request, 'Servicio creado exitosamente')
+            return redirect('servicios')  # Redirige a la vista de lista de servicios
         else:
             print(form.errors)
     else:
@@ -27,8 +29,8 @@ def servicio_create(request):
     context = {
         'form': form,
     }
-
-    return render(request, 'crearServicio.html', context)
+    print(servicio_list)
+    return render(request, 'crear_servicio.html', context)
 
 def home(request):
     # Aquí puedes colocar el código que deseas ejecutar cuando se visite la página de inicio
