6dea6beb6de5bdd84aeab084c2daa8b965d138ea
DanielAcevedo99 <69821353+DanielAcevedo99@users.noreply.github.com>
2023-10-21 05:03:23 -0500

    .

--- manejador_monitoreo/measurements/logic/logic_measurement.py
+++ manejador_monitoreo/measurements/logic/logic_measurement.py
@@ -1,18 +1,14 @@
 from ..models import Measurement
 
 def get_measurements():
-    queryset = Measurement.objects.all().order_by('-dateTime')[:10]
-    return (queryset)
+    queryset = Measurement.objects.all().order_by('-timestamp')[:10]
+    return queryset
 
 def create_measurement(form):
     measurement = form.save()
-    measurement.save()
-    return ()
+    return measurement
 
 def create_measurement_object(id, value, variable):
-    measurement = Measurement()
-    measurement.id = id
-    measurement.value = value
-    measurement.variable = variable
+    measurement = Measurement(id=id, value=value, variable=variable)
     measurement.save()
-    return ()
+    return measurement
