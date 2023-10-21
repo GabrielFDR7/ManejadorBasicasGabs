3b8e00cf7adf84a1913adbc80e190ec1a85e16cf
DanielAcevedo99 <69821353+DanielAcevedo99@users.noreply.github.com>
2023-10-21 10:14:05 -0500

    Update subscriber.py

--- manejador_monitoreo/subscriber.py
+++ manejador_monitoreo/subscriber.py
@@ -8,7 +8,7 @@ rabbit_host = '10.128.0.15'
 rabbit_user = 'rasi-db'
 rabbit_password = 'rasi2023'
 exchange = 'monitoring_measurements'
-topics = ['Patient.monitoring.#']
+topics = ['Patient.#']
 
 
 path.append('monitoring/settings.py')
@@ -48,7 +48,7 @@ def callback(ch, method, properties, body):
     if variable is not None:
         create_measurement_object(
             variable, payload['value'], payload['unit'], topic[0] + topic[1])
-        if variable.name == 'Heart-rate':
+        if variable.name == 'heart-rate':
             check_alarm(payload['value'])
         print("Measurement :%r" % (str(payload)))
     else:
