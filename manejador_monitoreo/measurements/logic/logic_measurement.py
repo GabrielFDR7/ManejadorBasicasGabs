30b62348bc845e8fc3a8c96de02bb7f763adb343
DanielAcevedo99 <69821353+DanielAcevedo99@users.noreply.github.com>
2023-10-21 03:39:43 -0500

    a

--- manejador_monitoreo/measurements/logic/logic_measurement.py
+++ manejador_monitoreo/measurements/logic/logic_measurement.py
@@ -1,12 +1,17 @@
 from ..models import Measurement
 
-def create_measurement(value, variable):
-    # Crea una nueva instancia de Measurement y la guarda en la base de datos
-    measurement = Measurement(value=value, variable=variable)
-    measurement.save()
-    return measurement
+def get_measurements():
+    queryset = Measurement.objects.all().order_by('-dateTime')[:10]
+    return (queryset)
 
-def get_latest_measurements(limit=10):
-    # Obtiene las últimas mediciones
-    measurements = Measurement.objects.order_by('-timestamp')[:limit]
-    return measurements
+def create_measurement(form):
+    measurement = form.save()
+    measurement.save()
+    return ()
+
+def create_measurement_object(variable_id, value, unit, place):
+    measurement = Measurement()
+    measurement.variable = variable_id
+    measurement.value = value
+    measurement.save()
+    return ()
