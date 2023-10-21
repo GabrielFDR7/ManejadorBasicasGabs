3409731bda4d00f0cc91cbc564c11e3939fc3ecc
DanielAcevedo99 <69821353+DanielAcevedo99@users.noreply.github.com>
2023-10-21 11:43:11 -0500

    Update settings.py

--- manejador_monitoreo/monitoring/settings.py
+++ manejador_monitoreo/monitoring/settings.py
@@ -127,5 +127,5 @@ EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'danielfelipe@gmail.com'
+EMAIL_HOST_USER = 'danielfelipe111@gmail.com'
 EMAIL_HOST_PASSWORD = 'zxyehekljpsaxapy'
